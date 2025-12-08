94eda43914c33106c4b2cbff9649a93e416f2462
Lars Kuizenga <larskuizenga@live.nl>
2025-12-08 21:06:45 +0100

    Day 8

--- Day8/Code.cs
+++ Day8/Code.cs
@@ -31,7 +31,7 @@ static class Code
 
     private static void PartTwo(string[] exampleInput, string[] input)
     {
-        ulong exampleInputAnswer = 1;
+        ulong exampleInputAnswer = 25272;
         ulong exampleOutput = SolvePartTwo(exampleInput);
 
         if (exampleOutput != exampleInputAnswer)
@@ -51,103 +51,161 @@ static class Code
         List<Connection> connections = [];
         List<Circuit> circuits = [];
 
-		junctionBoxes.AddRange(input.Select(line => new JunctionBox(line)));
+        junctionBoxes.AddRange(input.Select(line => new JunctionBox(line)));
+
+        foreach (JunctionBox junctionBox in junctionBoxes)
+        {
+            circuits.Add(new Circuit { JunctionBoxes = [junctionBox] });
+        }
+                
+        connections = Connection.SetConnections(junctionBoxes);
 
         for (int index = 0; index < connectionsToFind; index++)
         {
-	        Connection? shortestConnection = null;
+            Connection shortestConnection = connections[index];
 
-			foreach (JunctionBox leftJunctionBox in junctionBoxes)
-	        {
-		        foreach (JunctionBox rightJunctionBox in junctionBoxes)
-		        {
-			        if (connections.Any(c => c.LeftJunctionBox == leftJunctionBox && c.RightJunctionBox == rightJunctionBox))
-			        {
-                        continue;
-			        }
+            Circuit? circuitLeft = circuits.FirstOrDefault(c => c.JunctionBoxes.Any(jb => jb == shortestConnection.LeftJunctionBox));
+            Circuit? circuitRight = circuits.FirstOrDefault(c => c.JunctionBoxes.Any(jb => jb == shortestConnection.RightJunctionBox));
 
-			        double distance = 0; //Calculate distance between junction boxes
+            //Connection between junctionboxes of the same circuit
+            if (circuitLeft is not null && circuitRight is not null && circuitLeft == circuitRight) continue;
 
-			        if (shortestConnection is null)
-			        {
-				        shortestConnection = new Connection
-				        {
-							LeftJunctionBox = leftJunctionBox,
-							RightJunctionBox = rightJunctionBox,
-							Length = distance
-				        };
-
-                        continue;
-			        }
-
-			        if (distance < shortestConnection.Length)
-			        {
-				        shortestConnection = new Connection
-				        {
-					        LeftJunctionBox = leftJunctionBox,
-					        RightJunctionBox = rightJunctionBox,
-					        Length = distance
-				        };
-			        }
-
-		        }
-	        }
-            
-	        connections.Add(shortestConnection);
-
-			//Look for circuit on both sides of the connection
-			Circuit? circuit = circuits.FirstOrDefault(c => c.JunctionBoxes.Any(jb => jb == shortestConnection.LeftJunctionBox || jb == shortestConnection.RightJunctionBox));
-
-	        if (circuit is not null)
-	        {
-		        circuit.JunctionBoxes.Add(shortestConnection.LeftJunctionBox);
-		        circuit.JunctionBoxes.Add(shortestConnection.RightJunctionBox);
-	        }
-	        else
-	        {
-		        Circuit newCircuit = new Circuit();
-		        newCircuit.JunctionBoxes.Add(shortestConnection.LeftJunctionBox);
-		        newCircuit.JunctionBoxes.Add(shortestConnection.RightJunctionBox);
-
-		        circuits.Add(newCircuit);
-	        }
+            if (circuitLeft is not null && circuitRight is not null)
+            {
+                circuitLeft.JunctionBoxes.AddRange(circuitRight.JunctionBoxes);
+                circuitRight.JunctionBoxes.RemoveAll(jb => true);
+                circuits.Remove(circuitRight);
+            }
+            else if (circuitLeft is not null && circuitRight is null)
+            {
+                circuitLeft.JunctionBoxes.Add(shortestConnection.RightJunctionBox);
+            }
+            else if (circuitLeft is null && circuitRight is not null)
+            {
+                circuitRight.JunctionBoxes.Add(shortestConnection.LeftJunctionBox);
+            }
         }
 
-        return 0;
+        circuits = circuits.OrderByDescending(c => c.JunctionBoxes.Count).ToList();
+
+        return circuits[0].JunctionBoxes.Count * circuits[1].JunctionBoxes.Count * circuits[2].JunctionBoxes.Count;
     }
 
     private static ulong SolvePartTwo(string[] input)
     {
-        
+        List<JunctionBox> junctionBoxes = [];
+        List<Connection> connections = [];
+        List<Circuit> circuits = [];
 
-	    return 0;
+        junctionBoxes.AddRange(input.Select(line => new JunctionBox(line)));
+
+        foreach (JunctionBox junctionBox in junctionBoxes)
+        {
+            circuits.Add(new Circuit { JunctionBoxes = [junctionBox] });
+        }
+
+        connections = Connection.SetConnections(junctionBoxes);
+
+        Connection? shortestConnection = null;
+
+        int index = 0;
+        while (circuits.Count != 1)
+        {
+            shortestConnection = connections[index];
+            index++;
+
+            Circuit? circuitLeft = circuits.FirstOrDefault(c => c.JunctionBoxes.Any(jb => jb == shortestConnection.LeftJunctionBox));
+            Circuit? circuitRight = circuits.FirstOrDefault(c => c.JunctionBoxes.Any(jb => jb == shortestConnection.RightJunctionBox));
+
+            //Connection between junctionboxes of the same circuit
+            if (circuitLeft is not null && circuitRight is not null && circuitLeft == circuitRight) continue;
+
+            if (circuitLeft is not null && circuitRight is not null)
+            {
+                circuitLeft.JunctionBoxes.AddRange(circuitRight.JunctionBoxes);
+                circuitRight.JunctionBoxes.RemoveAll(jb => true);
+                circuits.Remove(circuitRight);
+            }
+            else if (circuitLeft is not null && circuitRight is null)
+            {
+                circuitLeft.JunctionBoxes.Add(shortestConnection.RightJunctionBox);
+            }
+            else if (circuitLeft is null && circuitRight is not null)
+            {
+                circuitRight.JunctionBoxes.Add(shortestConnection.LeftJunctionBox);
+            }
+        }
+
+        if (shortestConnection is null) throw new UnreachableException();
+
+        return ((ulong)shortestConnection.LeftJunctionBox.XPos) * ((ulong)shortestConnection.RightJunctionBox.XPos);
     }
 
+    [DebuggerDisplay("{XPos} {YPos} {ZPos}")]
     public class JunctionBox
     {
-        public int XPos { get; set; }
-        public int YPos { get; set; }
-        public int ZPos { get; set; }
+        public double XPos { get; set; }
+        public double YPos { get; set; }
+        public double ZPos { get; set; }
 
         public JunctionBox(string line)
         {
             string[] parts = line.Split(',');
 
-			XPos = int.Parse(parts[0]);
-            YPos = int.Parse(parts[1]);;
-            ZPos = int.Parse(parts[2]);;
-		}
-	}
+            XPos = double.Parse(parts[0]);
+            YPos = double.Parse(parts[1]); ;
+            ZPos = double.Parse(parts[2]); ;
+        }
+    }
 
+    [DebuggerDisplay("L: {LeftJunctionBox} | R: {RightJunctionBox} | {Length}")]
     public class Connection
     {
-        public JunctionBox LeftJunctionBox { get; set; }
-        public JunctionBox RightJunctionBox { get; set; }
+        public JunctionBox LeftJunctionBox { get; set; } = null!;
+        public JunctionBox RightJunctionBox { get; set; } = null!;
         public double Length { get; set; }
-	}
 
+        public static List<Connection> SetConnections(List<JunctionBox> junctionBoxes)
+        {
+            List<Connection> connections = [];
+
+            for (int leftJuctionBoxIndex = 0; leftJuctionBoxIndex < junctionBoxes.Count; leftJuctionBoxIndex++)
+            {
+                JunctionBox leftJunctionBox = junctionBoxes[leftJuctionBoxIndex];
+
+                for (int rightJunctionBoxIndex = leftJuctionBoxIndex + 1; rightJunctionBoxIndex < junctionBoxes.Count; rightJunctionBoxIndex++)
+                {
+                    if (rightJunctionBoxIndex == leftJuctionBoxIndex) continue;
+
+                    JunctionBox rightJunctionBox = junctionBoxes[rightJunctionBoxIndex];
+
+                    double distance = GetDistance(leftJunctionBox, rightJunctionBox);
+
+                    connections.Add(new Connection
+                    {
+                        LeftJunctionBox = leftJunctionBox,
+                        RightJunctionBox = rightJunctionBox,
+                        Length = distance,
+                    });
+                }
+            }
+
+            return connections.OrderBy(c => c.Length).ToList();
+        }
+
+        public static double GetDistance(JunctionBox leftJunctionBox, JunctionBox rightJunctionBox)
+        {
+            double x = Math.Pow(Math.Abs(leftJunctionBox.XPos - rightJunctionBox.XPos), 2);
+            double y = Math.Pow(Math.Abs(leftJunctionBox.YPos - rightJunctionBox.YPos), 2);
+            double z = Math.Pow(Math.Abs(leftJunctionBox.ZPos - rightJunctionBox.ZPos), 2);
+
+            return Math.Sqrt(x + y + z);
+        }
+    }
+
+    [DebuggerDisplay("Count = {JunctionBoxes.Count}")]
     public class Circuit
     {
-	    public List<JunctionBox> JunctionBoxes { get; set; } = [];
+        public List<JunctionBox> JunctionBoxes { get; set; } = [];
     }
 }
